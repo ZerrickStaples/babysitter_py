f7cd2e17b6f1a81bf8a4c72866fb5b1e9e81f884
Zerrick Staples <zerrickstaples@Zerricks-MacBook-Pro.local>
2020-05-16 10:17:52 -0400

    Wrote and passed test for restricted start time to 5 PM

--- babysitter.py
+++ babysitter.py
@@ -1,2 +1,5 @@
 def calculate_hours(start_time, end_time):
+    if start_time < 17:
+        start_time = 17
+        
     return end_time - start_time
--- test_babysitter.py
+++ test_babysitter.py
@@ -2,3 +2,6 @@ from babysitter import *
 
 def test_calculate_hours():
     assert calculate_hours(17, 18) == 1
+
+def test_earliest_start_time():
+    assert calculate_hours(16, 18) == 1
